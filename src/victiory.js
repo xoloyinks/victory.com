6f606aacd5bb17b411d67fe7c81d4af4d7d5dab3
Kolawole Moses <xoloyinks@gmail.com>
2023-03-11 00:02:23 -0800

    Resized

--- src/victiory.js
+++ src/victiory.js
@@ -3,8 +3,8 @@ import {FaBars} from 'react-icons/fa'
 export default function Victory() {
   return (
     <>
-      <section className='w-screen border-2 border-black'>
-          <nav className='bg-gray-800 flex justify-between text-white font-semibold px-5 py-3 text-2xl'>
+      <section className='w-screen'>
+          <nav className='bg-gray-800 flex justify-between text-white font-semibold px-5 py-3 text-xl'>
             <div>Olayinka</div>
             <div><FaBars /></div>
           </nav>
