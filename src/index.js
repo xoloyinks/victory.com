2b660c899781ae8c757ef20176bf831d02b8c9ad
Kolawole Moses <xoloyinks@gmail.com>
2023-03-10 23:54:55 -0800

    Finally Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './input.css';
+import './output.css';
 import reportWebVitals from './reportWebVitals';
 import Victory from './victiory';
 
