03fa0e6ea3a4ba70ce023e53bcdf8ec06d595799
Kolawole Moses <xoloyinks@gmail.com>
2023-03-10 23:42:20 -0800

    Finally

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './input.css';
 import reportWebVitals from './reportWebVitals';
 import Victory from './victiory';
 
--- src/victiory.js
+++ src/victiory.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import {FaBars} from 'react-icons/fa'
 export default function Victory() {
   return (
-    <div>victory</div>
+    <>
+      <section className='w-screen border-2 border-black'>
+          <nav className='bg-gray-800 flex justify-between text-white font-semibold px-5 py-3 text-2xl'>
+            <div>Olayinka</div>
+            <div><FaBars /></div>
+          </nav>
+      </section>
+    </>
   )
 }
