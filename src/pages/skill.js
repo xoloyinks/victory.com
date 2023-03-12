e087043ef8e53b6af5c90252673c662606bbd4d5
Kolawole Moses <xoloyinks@gmail.com>
2023-03-12 00:55:06 -0800

    Commit skills

---
+++ src/pages/skill.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Skill() {
+  return (
+    <>
+        <section className='w-screen h-screen bg-slate-400'>
+
+        </section>
+    </>
+  )
+}
