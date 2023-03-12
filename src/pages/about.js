b8cb03f4aabd158c7e3f9fa13da32d3f3f1886bc
Kolawole Moses <xoloyinks@gmail.com>
2023-03-11 18:24:49 -0800

    Pages

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+        <section className='px-5 w-screen h-screen'>
+            <div className='text-5xl font-semibold pt-28 leading-normal'>
+                <pre>
+                    Hey There, <br />
+                    I'm Kolawole
+                </pre>
+            </div>
+            <div className='text-lg font-semibold'>
+                <p>I build Fast, Generic, and Responsive Websites.</p>
+            </div>
+        </section>
+    </>
+  )
+}
