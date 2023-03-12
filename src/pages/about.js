67548bfdfdd82b00706361e710c9f0e8c390c597
Kolawole Moses <xoloyinks@gmail.com>
2023-03-11 18:31:24 -0800

    Done

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,14 @@ export default function About() {
   return (
     <>
         <section className='px-5 w-screen h-screen'>
-            <div className='text-5xl font-semibold pt-28 leading-normal'>
+            <div className='text-5xl font-semibold pt-28 leading-sung'>
                 <pre>
                     Hey There, <br />
                     I'm Kolawole
                 </pre>
             </div>
-            <div className='text-lg font-semibold'>
+            <br />
+            <div className='text-lg'>
                 <p>I build Fast, Generic, and Responsive Websites.</p>
             </div>
         </section>
