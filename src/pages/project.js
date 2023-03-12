5bdf7bffac5abd398f614ca692fa8a74e4fda244
Kolawole Moses <xoloyinks@gmail.com>
2023-03-12 04:14:21 -0700

    Project

---
+++ src/pages/project.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import skill from "./styles/images/icons8-skills.png";
+
+export default function Project() {
+  return (
+    <>
+        <section className='w-screen h-screen px-5 py-5'>
+            <h1 className='text-3xl text-black flex items-center  gap-4'>
+                My Projects <span className='font-bold'></span>
+            </h1><br />
+        </section>
+    </>
+  )
+}
