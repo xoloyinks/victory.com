ba44491d7243a9a253660680584ef1fc8eb1d5bb
Kolawole Moses <xoloyinks@gmail.com>
2023-03-12 00:55:30 -0800

    Done

--- src/victiory.js
+++ src/victiory.js
@@ -2,16 +2,18 @@ import React from 'react'
 import {FaBars} from 'react-icons/fa';
 import {BsFillMoonFill} from 'react-icons/bs'
 import About from './pages/about';
+import Skill from './pages/skill';
 export default function Porfolio() {
   return (
     <>
       <section className='w-screen relative'>
       
-          <nav className='z-50 flex items-center text-black justify-between fixed backdrop-blur-xl bg-red/30 w-full px-5 py-5'>
+          <nav className='z-50 flex items-center text-black justify-between fixed backdrop-blur-xl bg-white/30 w-full px-5 py-5'>
             <div className='logo text-2xl'>Kolawole</div>
             <div className='flex justify-between w-2/12 text-xl'><span><BsFillMoonFill /></span><span><FaBars /></span></div>
           </nav>
           <About />
+          <Skill />
       </section>
     </>
   )
