d8741bfd89f9f817ce137194a0044c6553dfe4d9
Kolawole Moses <xoloyinks@gmail.com>
2023-03-12 01:45:55 -0800

    Done

--- src/pages/skill.js
+++ src/pages/skill.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import skill from "./styles/images/icons8-skills.png"
 
 export default function Skill() {
   return (
     <>
-        <section className='w-screen h-screen bg-slate-400'>
-
+        <section className='w-screen h-screen bg-slate-400 px-5 py-5'>
+            <h1 className='text-3xl text-white flex items-center  gap-4'>
+                My Skills <span className='font-bold'> <img src={skill} alt="" className='w-[40px] animate-bounce'/></span>
+            </h1>
         </section>
     </>
   )
